a3db54e8bcffd775fc21248d212e93d9a466c796
muneeb203 <149325761+muneeb203@users.noreply.github.com>
2024-07-04 16:33:58 +0500

    Add files via upload

---
+++ displaying pattern with setw.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+cout<<setw(52)<<"_________________________________________"<<endl;
+cout<<setw(11)<<"/"<<setw(0)<<"\\"<<setw(41)<<"\\"<<endl;
+cout<<setw(10)<<"/"<<setw(3)<<"\\"<<setw(41)<<"\\"<<endl;
+cout<<setw(9)<<"/"<<setw(5)<<"\\"<<setw(41)<<"\\"<<endl;
+cout<<setw(8)<<"/"<<setw(7)<<"\\"<<setw(24)<<"________"<<setw(17)<<"\\"<<endl;
+cout<<setw(7)<<"/"<<setw(9)<<"\\"<<setw(16)<<"\\"<<setw(4)<<"\\"<<setw(4)<<"\\"<<setw(17)<<"\\"<<endl;
+cout<<setw(6)<<"/"<<setw(11)<<"\\"<<setw(16)<<"\\"<<setw(0)<<"==="<<setw(0)<<"\\"<<setw(0)<<"==="<<setw(0)<<"\\"<<setw(17)<<"\\"<<endl;
+cout<<setw(5)<<"/"<<setw(13)<<"\\"<<setw(16)<<"\\"<<"___"<<"\\"<<"___"<<"\\"<<setw(17)<<"\\"<<endl;
+cout<<setw(4)<<"/"<<setw(15)<<"\\"<<setw(41)<<"\\"<<endl;
+cout<<setw(3)<<"/"<<setw(17)<<"\\"<<setw(41)<<"\\"<<endl;
+cout<<setw(2)<<"/"<<"__________________"<<"\\"<<setw(41)<<"\\"<<endl;
+cout<<setw(1)<<"/"<<"|"<<setw(19)<<"|"<<"\\"<<"________________________________________"<<"\\"<<endl;
+cout<<setw(2)<<"|"<<setw(19)<<"|"<<setw(41)<<"|"<<endl;
+cout<<setw(2)<<"|"<<setw(19)<<"|"<<setw(41)<<"|"<<endl;
+cout<<setw(2)<<"|"<<setw(19)<<"|"<<setw(41)<<"|"<<endl;
+cout<<setw(2)<<"|"<<setw(12)<<"_______"<<setw(7)<<"|"<<setw(22)<<"_____"<<setw(19)<<"|"<<endl;
+cout<<setw(2)<<"|"<<setw(6)<<"|"<<setw(6)<<"|"<<setw(7)<<"|"<<setw(18)<<"|"<<"_"<<"|"<<"_"<<setw(0)<<"|"<<setw(19)<<"|"<<endl;
+cout<<setw(2)<<"|"<<setw(6)<<"|"<<setw(6)<<"|"<<setw(7)<<"|"<<setw(18)<<"|"<<"_"<<"|"<<"_"<<setw(0)<<"|"<<setw(19)<<"|"<<endl;
+cout<<setw(2)<<"|"<<setw(6)<<"|"<<setw(6)<<"|"<<setw(7)<<"|"<<setw(41)<<"|"<<endl;
+cout<<setw(2)<<"|"<<setw(6)<<"|"<<setw(5)<<"+"<<"|"<<setw(7)<<"|"<<setw(41)<<"|"<<endl;
+cout<<setw(2)<<"|"<<setw(6)<<"|"<<setw(6)<<"|"<<setw(7)<<"|"<<setw(41)<<"|"<<endl;
+cout<<setw(2)<<"|"<<"_____"<<"|"<<"_____"<<"|"<<"______"<<"|"<<"________________________________________"<<"|"<<endl;
+return 0;
+}
---
+++ printing stars .cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+int main()
+{
+cout<<setw(5)<<"*"<<endl;
+cout<<setw(6)<<"***"<<endl;
+cout<<setw(7)<<"*****"<<endl;
+cout<<setw(6)<<"***"<<endl;
+cout<<setw(5)<<"*"<<endl;
+cout<<"\n\n\n";
+cout<<setw(12)<<"**********"<<endl;
+cout<<setw(4)<<"*"<<setw(12)<<"*"<<endl;
+cout<<setw(4)<<"*"<<setw(12)<<"*"<<endl;
+cout<<setw(4)<<"*"<<setw(12)<<"*"<<endl;
+cout<<setw(12)<<"**********"<<endl;
+cout<<"\n\n\n";
+cout<<setw(3)<<"*"<<endl;
+cout<<setw(1)<<"**"<<endl;
+cout<<setw(4)<<"***"<<endl;
+cout<<setw(4)<<"****"<<endl;
+cout<<setw(5)<<"*****"<<endl;
+cout<<"\n\n\n";
+cout<<setw(1)<<"*"<<endl;
+cout<<setw(5)<<"**"<<endl;
+cout<<setw(5)<<"***"<<endl;
+cout<<setw(4)<<"****"<<endl;
+cout<<setw(5)<<"*****"<<endl;
+cout<<"\n\n\n";
+cout<<setw(1)<<"*"<<setw(7)<<"*"<<endl;
+cout<<setw(1)<<"*"<<setw(6)<<"*"<<endl;
+cout<<setw(5)<<"*"<<endl;
+cout<<setw(1)<<"*"<<setw(6)<<"*"<<endl;
+cout<<setw(1)<<"*"<<setw(7)<<"*"<<endl;
+cout<<"\n\n\n";
+cout<<"*********"<<endl;
+cout<<"*********"<<endl;
+cout<<"*********"<<endl;
+cout<<"*********"<<endl;
+cout<<"*********"<<endl;
+
+
+
+
+return 0;
+}
