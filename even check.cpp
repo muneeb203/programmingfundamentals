8944b39b77d89e6c4c00dd4960162e7392b03892
muneeb203 <149325761+muneeb203@users.noreply.github.com>
2024-07-07 17:02:13 +0500

    Add files via upload

---
+++ even check.cpp
@@ -0,0 +1,17 @@
+//Muneeb ur Rehman Quresi
+//DS-B
+//22I-1965
+
+#include<iostream>
+using namespace std;
+int main(){
+int x;
+cout<<"Enter the no=";
+cin>>x;
+if(x&1)
+cout<<"Entered no is odd";
+else 
+cout<<"Entered no is even";
+
+return 0;
+}
---
+++ lowercase letter check.cpp
@@ -0,0 +1,21 @@
+//Muneeb ur Rehman Quresi
+//DS-B
+//22I-1965
+
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    char x;
+    cout<<"Enter the character="<<endl;
+    cin>>x;
+    if ((x>=97)&&(x<=112))
+    cout<<"Character is a lower case letter."<<endl;
+    
+    else 
+    cout<<"Character is not a lower case letter."<<endl;
+
+    return 0;
+}
